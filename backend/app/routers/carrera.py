f0369949727fdc9fe3cdc76bb189de2b8e3ddae5
kami <kamilahurtado31@gmail.com>
2026-08-19 16:11:31 -0500

    Modificacion

--- backend/app/routers/carrera.py
+++ backend/app/routers/carrera.py
@@ -2,4 +2,5 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 from database import get_session
 
-Router_carrera = APIRouter(prefix="/users", tags=["Inicio sesion"])
+Router_carrera = APIRouter(prefix="/user", tags=["Inicio de sesion"])
+
